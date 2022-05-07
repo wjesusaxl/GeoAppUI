54b26b32e07ca8cb318c666238b82441ba681b34
Jesus Rodriguez <wjesus_axl@hotmail.com>
2022-05-06 23:50:15 -0500

    Adding GeoApp Logo to Login Feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './features/login/pages/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: MainComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/features/features.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainComponent } from './login/pages/main/main.component';
+
+
+
+@NgModule({
+  declarations: [
+    MainComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FeaturesModule { }
